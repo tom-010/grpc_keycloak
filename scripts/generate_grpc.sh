08ec4c03e3f91e9c66854d18e4ca05242665d62b
Thomas Deniffel <tdeniffel@gmail.com>
2022-05-24 10:58:29 +0200

    unencryped/unauthenticated

---
+++ scripts/generate_grpc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+protoc \
+    --go_out=. \
+    --go_opt=paths=source_relative \
+    --go-grpc_out=. \
+    --go-grpc_opt=paths=source_relative \
+    proto/users.proto
+
+# protoc \
+#     --dart_out=grpc:fe/dart/lib/src/gen \
+#     -Iproto proto/users.proto
--- server/server.go
+++ server/server.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"math/rand"
+	"net"
 
+	pb "deniffel.com/grpc_keycloak/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+type UserManagementServer struct {
+	pb.UnimplementedUserManagementServer
+}
+
+func (s *UserManagementServer) CreateNewUser(ctx context.Context, in *pb.NewUser) (*pb.User, error) {
+	log.Printf("Received: %v", in.GetName())
+	var user_id int32 = int32(rand.Intn(100000))
+	return &pb.User{Name: in.GetName(), Age: in.GetAge(), Id: user_id}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listn: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterUserManagementServer(s, &UserManagementServer{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
